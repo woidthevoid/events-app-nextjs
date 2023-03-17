b5b7092d6b27af0daf2c68b6e94e3af62e04c1f3
Victor <woydowskidralle@gmail.com>
2023-03-17 19:09:20 +0100

    Data manament

--- src/components/events/single-event.jsx
+++ src/components/events/single-event.jsx
@@ -1,12 +1,42 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import Image from 'next/image'
+import { useRouter } from 'next/router';
 
 export const SingleEvent = ({ data }) => {
-    const inputEmail = useRef;
-    const onSubmit = (e) => {
+    const inputEmail = useRef();
+    const router = useRouter();
+    const [message, setMessage] = useState(' ');
+
+    const onSubmit = async (e) => {
         e.preventDefault();
-        
+        const emailValue = inputEmail.current.value;
+        const eventId = router?.query.id;
+
+        const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+        if (!emailValue.match(validRegex)) {
+            setMessage('Please add a valid email address')
+        }
+
+        try {
+            const response = await fetch('/api/email-registration', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ email: emailValue, eventId })
+            });
+
+            if (!response.ok) throw new Error(`Error: ${response.status}`);
+            const data = await response.json();
+            setMessage(data.message);
+            inputEmail.current.value = '';
+
+        } catch (e) {
+            console.log(e, 'Error');
+        }
     };
+
     return (
         <div className='event_single_page'>
             <h1>{data.title}</h1>
@@ -14,9 +44,10 @@ export const SingleEvent = ({ data }) => {
             <p>{data.description}</p>
             <form onSubmit={onSubmit} className='email_registration'>
                 <label>Register here!</label>
-                <input ref={inputEmail} type="email" id='email' placeholder='Enter your email here' />
-                <button type='button'>Submit</button>
+                <input ref={inputEmail} id='email' placeholder='Enter your email here' />
+                <button type='submit'>Submit</button>
             </form>
+            <p className='message'>{message}</p>
         </div>
     )
 }
